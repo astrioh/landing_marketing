4f6f8272b8affc33844079895201c1762cafd6ca
Ilya Butskikh <libut0853@gmail.com>
2021-12-02 14:02:53 +0300

    Добавлена секция преимуществ и модальное окно

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-const btnRequest = document.getElementsByClassName('button_primary');
+const openModal = () => {
+  // ....
+  const modal = document.querySelector('.modal');
+  modal.style.display = 'block';
+};
+
+const closeModal = () => {
+  const modal = document.querySelector('.modal');
+  modal.style.display = 'none';
+};
+
+const requestBtns = document.querySelectorAll('.js-open-modal');
+// .js-close-modal
+
+[...requestBtns].forEach((requestBtn) => {
+  requestBtn.addEventListener('click', openModal);
+});
